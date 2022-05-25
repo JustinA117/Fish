b34cc54ae8b68ad042cec0072d62858554c06089
JustinA117 <94567481+JustinA117@users.noreply.github.com>
2022-05-25 12:15:05 -0700

    Create GameRunner.java

---
+++ GameRunner.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+
+public class GameRunner
+{
+  public static void main(String[] args)
+  {
+    
+    ArrayList<String> fishMain = Fish.AddFish("Salmon");
+    fishMain = Fish.AddFish("Walrus");
+    String bannana = Fish.PrintFishes(fishMain);
+    System.out.println(bannana);
+    int num = Fish.GetNumFishes(fishMain);
+    System.out.println("There are " + num + " fishes");
+  }
+
+  
+  
+}
