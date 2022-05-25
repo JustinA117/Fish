366e1553c8394fada470797c994da51e3e3cf374
JustinA117 <94567481+JustinA117@users.noreply.github.com>
2022-05-25 12:24:50 -0700

    Update Fish.java

--- Fish.java
+++ Fish.java
@@ -2,20 +2,22 @@ import java.util.ArrayList;
 
 public class Fish
 {
-
+// The initialization of the variables
   static ArrayList<String> Fish = new ArrayList<String>();
   int numFishes = 0;
-  
-    public static int GetNumFishes(ArrayList<String> Fish)
+  //returns how manu Strings are in the array
+    public static int GetNumFishes(ArrayList<String> Fish) 
   {
     int numFishes = Fish.size();
     return numFishes;
   }
+  // add a String that contains a fish to the array
   public static ArrayList<String> AddFish(String fishName)
   {
     Fish.add(fishName);
     return Fish;
   }
+  // Returns one big String of each singular String in the array separated by a comma
   public static String PrintFishes(ArrayList<String> Fish)
   {
     String x = "";
